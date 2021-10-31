0c0a51a4650a33b647ab2e6820343b6949008be0
Karlheinz <kmachill@t-online.de>
2021-10-31 12:29:03 +0100

    Debug Mode Banner entfernt

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Advicer',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
