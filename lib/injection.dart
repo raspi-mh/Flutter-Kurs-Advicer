af577b4928291f8397dc6553280983f3b01b6f2f
Karlheinz <kmachill@t-online.de>
2021-11-09 21:40:45 +0100

    TDD - theme service UI integration

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,40 +1,45 @@
+import 'package:advicer/application/advicer/advicer_bloc.dart';
 import 'package:advicer/application/theme/theme_service.dart';
+import 'package:advicer/domain/repositories/advicer_repository.dart';
 import 'package:advicer/domain/repositories/theme_repository.dart';
+import 'package:advicer/domain/usecases/advicer_usecases.dart';
+import 'package:advicer/infrastructure/datasources/advicer_remote_datasource.dart';
 import 'package:advicer/infrastructure/datasources/theme_local_datasource.dart';
+import 'package:advicer/infrastructure/repositories/advicer_repository_impl.dart';
 import 'package:advicer/infrastructure/repositories/theme_repository_impl.dart';
 import 'package:get_it/get_it.dart';
+
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'application/advicer/advicer_bloc.dart';
-import 'domain/repositories/advicer_repository.dart';
-import 'domain/usecases/advicer_usecases.dart';
-import 'infrastructure/datasources/advicer_remote_datasource.dart';
-import 'infrastructure/repositories/advicer_repository_impl.dart';
 
-final sl = GetIt.I; // sl == service locater
+final sl = GetIt.I; // sl == service Locator
 
 Future<void> init() async {
-//! applicatioon layer
+  //! application layer
   sl.registerFactory(() => AdvicerBloc(usecases: sl()));
-  sl.registerLazySingleton<ThemeService>(() => ThemeServiceImpl(themeRepository: sl()));
+  sl.registerLazySingleton<ThemeService>(
+      () => ThemeServiceImpl(themeRepository: sl()));
 
   //! Usecases
   sl.registerLazySingleton(() => AdvicerUsecases(advicerRepository: sl()));
 
   //! repos
+
   sl.registerLazySingleton<AdvicerRepository>(
       () => AdvicerRepositoryImpl(advicerRemoteDatasource: sl()));
   sl.registerLazySingleton<ThemeRepository>(
       () => ThemeRepositoryImpl(themeLocalDatasource: sl()));
 
   //! datasources
+
   sl.registerLazySingleton<AdvicerRemoteDatasource>(
       () => AdvicerRemoteDatasourceImpl(client: sl()));
-  sl.resetLazySingleton<ThemeLocalDatasource>(
-      instance: () => ThemeLocalDatasourceImpl(sharedPreferences: sl()));
+  sl.registerLazySingleton<ThemeLocalDatasource>(
+      () => ThemeLocalDatasourceImpl(sharedPreferences: sl()));
 
   //! extern
+
   sl.registerLazySingleton(() => http.Client());
-  final sharedPreferences = await SharedPreferences.getInstance();
-  sl.registerLazySingleton(() => sharedPreferences);
+  final sharedPrefernces = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPrefernces);
 }
--- lib/presentation/advicer/advicer_page.dart
+++ lib/presentation/advicer/advicer_page.dart
@@ -1,10 +1,12 @@
 //import 'package:advicer/application/advicer/advicer_bloc.dart';
 import 'package:advicer/application/advicer/advicer_bloc.dart';
+import 'package:advicer/application/theme/theme_service.dart';
 import 'package:advicer/presentation/advicer/widgets/advice_field.dart';
 import 'package:advicer/presentation/advicer/widgets/custom_button.dart';
 import 'package:advicer/presentation/advicer/widgets/error_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 class AdvicerPage extends StatelessWidget {
   const AdvicerPage({Key? key}) : super(key: key);
@@ -15,6 +17,14 @@ class AdvicerPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
           centerTitle: true,
+          actions: [
+            Switch(
+                value: Provider.of<ThemeService>(context).isDarkModeOn,
+                onChanged: (_) {
+                  Provider.of<ThemeService>(context, listen: false)
+                      .toggleTheme();
+                }),
+          ],
           title: Text(
             "Advicer",
             style: themeData.textTheme.headline1,
